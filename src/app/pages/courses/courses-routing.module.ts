89dda8b245397f9027799d0ba7ebaf741fa386a6
David Castrillón <david.castri@hotmail.es>
2021-10-26 15:11:35 +0200

    Routing fet pendent de prova amb mock

--- src/app/pages/courses/courses-routing.module.ts
+++ src/app/pages/courses/courses-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CoursesComponent } from './courses/courses.component';
 import { ModulesComponent } from '../modules/modules/modules.component';
+import { TopicsComponent } from '../topics/topics/topics.component';
 
 
 const routes: Routes = [
