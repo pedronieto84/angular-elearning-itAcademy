1871b9341e6eff78f3a8d47f9fa9a530b0598451
David Castrillón <david.castri@hotmail.es>
2021-10-26 18:58:42 +0200

    Prototipo de getUsers en data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,10 +1,57 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Module, Usuari } from '../interfaces/interfaces';
+
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class DataService {
+  /* PROPERTIES */
+  private baseUrl: string = 'https://us-central1-elearning-vue-itacademy.cloudfunctions.net'; //URL base on es fan les peticions
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /* GET: getUsers recupera tots els usuaris */
+  getUsers(): Observable<Usuari[]> {
+    return this.http.get<Usuari[]>(this.baseUrl);
+  }
+
+  /* GET: getCourses recupera tots els cursos */
+  getCourses() {
+
+  }
+
+  /* GET: getModules recupera tots els moduls */
+  getModules(): Observable<Module[]> {
+    return this.http.get<Module[]>(this.baseUrl + '/getModules');
+  }
+
+  /* GET: getTopics recupera tots els topics */
+  getTopics() {
+
+  }
+
+  /* DELETE: deleteUser elimina un usuari mitjançant la seva id */
+  deleteUser(userId: number) {
+
+  }
+
+  /* DELETE: deleteCourse elimina un curs mitjançant la seva id */
+  deleteCourse(courseId: number) {
+
+  }
+
+  /* DELETE: deleteModule elimina un mòdul mitjançant la seva id */
+  deleteModule(moduleId: number) {
+
+  }
+
+  /* DELETE: deleteTopic elimina un tòpic mitjançant la seva id */
+  deleteTopic(topicId: number) {
+    
+  }
 }
