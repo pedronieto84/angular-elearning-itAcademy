d7de6a8d963b22f25db1398d93a20ea2f0c7243e
Ezequiel Lo Iacono <ezequiel_lo_iacono@pm.me>
2021-11-10 20:26:27 +0100

    feat:created logical part of the table

--- src/app/pages/users/users/users.component.ts
+++ src/app/pages/users/users/users.component.ts
@@ -1,17 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
-
+import { User } from 'src/app/interfaces/interfaces';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.sass']
+  styleUrls: ['./users.component.sass'],
 })
-export class UsersComponent implements OnInit {
-  
-  constructor(private dataService: DataService) { }
+export class UsersComponent implements OnInit, AfterViewInit {
+  users!: User[];
+  displayedColumns: string[] = ['userId', 'UserName', 'userScore', 'email'];
+
+  dataSource: MatTableDataSource<User> = new MatTableDataSource(this.users);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    
-  }  
+    this.dataService
+      .getUsers()
+      .subscribe((response) => (this.users = response));
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
