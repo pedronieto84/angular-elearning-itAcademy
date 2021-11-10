c2e64d196d59d738abf0f554da73e2f0f3a51809
Ezequiel Lo Iacono <ezequiel_lo_iacono@pm.me>
2021-11-10 20:25:37 +0100

    aggregated module and imported material

--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users/users.component';
 import { UsersRoutingModule } from './users-routing.module';
-
-
+import { MaterialModule } from 'src/app/material/material.module';
 
 @NgModule({
-  declarations: [
-    UsersComponent
-  ],
-  imports: [
-    CommonModule,
-    UsersRoutingModule
-  ]
+  declarations: [UsersComponent],
+  imports: [CommonModule, UsersRoutingModule, MaterialModule],
 })
-export class UsersModule { }
+export class UsersModule {}
