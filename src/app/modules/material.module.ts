77cbe88c1857ce552f1d0a6533c21ab53a1291f2
Ezequiel Lo Iacono <ezequiel_lo_iacono@pm.me>
2021-11-19 20:26:05 +0100

    feat: Added SnackBarModule

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 @NgModule({
   declarations: [],
   imports: [CommonModule],
@@ -18,6 +19,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatInputModule,
     ReactiveFormsModule,
     MatButtonModule,
+    MatSnackBarModule,
   ],
 })
 export class MaterialModule {}
