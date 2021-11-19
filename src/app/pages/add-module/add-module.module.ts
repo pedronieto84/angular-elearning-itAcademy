b57a5077f8d42d18b136067b60f14b0c6521a4b9
Ezequiel Lo Iacono <ezequiel_lo_iacono@pm.me>
2021-11-19 13:30:20 +0100

    feat: Import material module

--- src/app/pages/add-module/add-module.module.ts
+++ src/app/pages/add-module/add-module.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddModuleComponent } from './add-module/add-module.component';
 import { AddModuleRoutingModule } from './add-module-routing.module';
-
-
+import { MaterialModule } from 'src/app/material/material.module';
 
 @NgModule({
-  declarations: [
-    AddModuleComponent
-  ],
-  imports: [
-    CommonModule,
-    AddModuleRoutingModule
-  ]
+  declarations: [AddModuleComponent],
+  imports: [CommonModule, AddModuleRoutingModule, MaterialModule],
 })
-export class AddModuleModule { }
+export class AddModuleModule {}
